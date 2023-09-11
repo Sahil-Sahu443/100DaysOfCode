5901976646f286abd41873ae0c96af6e3c09ea66
Som <sahilsahu6032@gmail.com>
2023-09-11 20:22:05 +0530

    Day 1 of Challenge

---
+++ D_01.js
@@ -0,0 +1,35 @@
+// How to print
+// console.log('Sikh Le Javascript Bae')
+// How to declare Variable 
+var a = 'SOM OP' 
+// console.log(a)
+a=10
+// console.log(a)
+// ---------------------------------------
+
+// Difference between Var,const and let
+// console.log('uses of variables')
+// var a= 8
+// var a=6    {it cannot be changed so it is not efficient to use}
+let b = "SOM";
+
+
+const title = 'SS';
+console.log(title)
+
+// title=Ss    {Throws an error because constant cannot be chamnged}
+
+{
+    let b = "SAHIL"
+    const sahu = 'Sahil'
+    console.log(b)
+    console.log(sahu)
+}
+console.log(b)
+
+const func = () => {
+    let b = 458629
+    console.log(b)
+}
+
+func()
