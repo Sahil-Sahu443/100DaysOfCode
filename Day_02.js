d2f62ddebfafac36a1b60d45a11542466dda9310
Som <sahilsahu6032@gmail.com>
2023-09-12 19:32:08 +0530

    Day2 of 100DaysOfCode

---
+++ Day_02.js
@@ -0,0 +1,23 @@
+// Conditional expression 
+let a = 10;
+if(a>8){
+    console.log("this is age of greater than 8")
+}
+else{
+    console.log("this is age of less than 8")
+}
+
+switch (a) {
+    case a<10:
+        console.log("a is less than 10")
+        break;
+    
+    case a=10:
+        console.log("a = 10")
+        break;
+
+    default:
+        break;
+}
+
+// a < 5 ? console.log("a < 8") : console.log("a > 8")
